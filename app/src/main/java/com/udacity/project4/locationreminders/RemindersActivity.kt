347b8f1444a1444fe388a9f8d45a20cecc5fd773
Jose Villalobos Martinez <villalobosmartinezjosedavid@gmail.com>
2024-10-04 00:44:31 -0600

    Fixed scenario 4, automatic geofence once the location permission and the location dialog enables location

--- app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -1,17 +1,18 @@
 package com.udacity.project4.locationreminders
 
 import android.Manifest
-import android.annotation.TargetApi
+import android.app.Activity
 import android.app.PendingIntent
 import android.content.Intent
-import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.GeofencingClient
@@ -23,7 +24,6 @@ import com.google.android.material.snackbar.Snackbar
 import com.udacity.project4.R
 import com.udacity.project4.authentication.AuthenticationActivity
 import com.udacity.project4.databinding.ActivityRemindersBinding
-import com.udacity.project4.utils.REQUEST_TURN_DEVICE_LOCATION_ON
 
 /**
  * The RemindersActivity that holds the reminders fragments
@@ -31,12 +31,28 @@ import com.udacity.project4.utils.REQUEST_TURN_DEVICE_LOCATION_ON
 class RemindersActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRemindersBinding
     private lateinit var geofencingClient: GeofencingClient
+    private lateinit var resolutionForResultLauncher: ActivityResultLauncher<IntentSenderRequest>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRemindersBinding.inflate(layoutInflater)
         setContentView(binding.root)
         geofencingClient = LocationServices.getGeofencingClient(this)
+
+        resolutionForResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartIntentSenderForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                // Location was enabled
+                println("Location was enabled, calling additional function")
+                startGeofenceCallback?.invoke()
+            } else {
+                // Location was not enabled
+                println("Location was not enabled")
+                // Handle the case where location wasn't enabled
+                Toast.makeText(this, R.string.location_required_error, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -54,30 +70,6 @@ class RemindersActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
-
-//    @TargetApi(29)
-//    fun foregroundLocationPermissionApproved(): Boolean {
-//        val foregroundLocationApproved = (
-//                PackageManager.PERMISSION_GRANTED ==
-//                        ActivityCompat.checkSelfPermission(
-//                            this,
-//                            Manifest.permission.ACCESS_FINE_LOCATION
-//                        ))
-//        return foregroundLocationApproved
-//    }
-//
-//    @TargetApi(29)
-//    fun requestForegroundLocationPermission() {
-//        if (foregroundLocationPermissionApproved())
-//            return
-//        val permissionsArray = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
-//        val resultCode = REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
-//        ActivityCompat.requestPermissions(
-//            this@RemindersActivity,
-//            permissionsArray,
-//            resultCode
-//        )
-//    }
 
     private fun showSnackBar(message: String) {
         Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG).show()
@@ -105,10 +97,14 @@ class RemindersActivity : AppCompatActivity() {
             }
         }
     }
+
+    private var startGeofenceCallback: (() -> Unit)? = null
+
     fun checkDeviceLocationSettingsAndStartGeofence(
         resolve: Boolean = true,
-        onCompleteCallback: () -> Unit
+        startGeofenceCallback: () -> Unit
     ) {
+        this.startGeofenceCallback = startGeofenceCallback
         val locationRequest = LocationRequest.create().apply {
             priority = LocationRequest.PRIORITY_LOW_POWER
         }
@@ -120,23 +116,15 @@ class RemindersActivity : AppCompatActivity() {
         println("prueba, adding failure listener to location on/off task")
         locationSettingsResponseTask.addOnFailureListener { exception ->
             if (exception is ResolvableApiException && resolve) {
-                println("prueba,location task exception, trying to resolute")
-                try {
-                    exception.startResolutionForResult(
-                        this,
-                        REQUEST_TURN_DEVICE_LOCATION_ON
-                    )
-                } catch (sendEx: IntentSender.SendIntentException) {
-                    println("prueba,location task exception, failed to resolute")
-                    // show snackbar error
-                }
+                val intentSenderRequest = IntentSenderRequest.Builder(exception.resolution).build()
+                resolutionForResultLauncher.launch(intentSenderRequest)  // Launch
             } else {
                 println("prueba,location task failure, rejected by user, showing snackbar")
                 Snackbar.make(
                     binding.root,
                     R.string.location_required_error, Snackbar.LENGTH_INDEFINITE
                 ).setAction(android.R.string.ok) {
-                    checkDeviceLocationSettingsAndStartGeofence(onCompleteCallback = onCompleteCallback)
+                    checkDeviceLocationSettingsAndStartGeofence(startGeofenceCallback = startGeofenceCallback)
                 }.show()
             }
         }
@@ -144,7 +132,7 @@ class RemindersActivity : AppCompatActivity() {
             //#3 Automatically attempt to add geofence if we are certain that device location is on
             if (it.isSuccessful) {
                 println("prueba,location task success, calling callback")
-                onCompleteCallback()
+                startGeofenceCallback()
             }
         }
     }
